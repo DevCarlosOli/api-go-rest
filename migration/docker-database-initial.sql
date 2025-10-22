0b04e1fe54f121b516261ae54a03b5354e0731ca
Carlos Oliveira <c.oliveiradev@gmail.com>
2025-10-21 22:54:06 -0300

    Initial commit

---
+++ migration/docker-database-initial.sql
@@ -0,0 +1,17 @@
+CREATE TABLE PERSONALIDADES(
+    ID SERIAL PRIMARY KEY,
+    NOME VARCHAR,
+    HISTORIA VARCHAR
+);
+
+INSERT INTO PERSONALIDADES(
+    NOME, 
+    HISTORIA
+    ) VALUES (
+        'Deodato Petit Wertheimer',
+        'Deodato Petit Wertheimer foi um médico e político brasileiro, seus primeiros anos de vida foram em São Paulo, mas logo mudou para Nova Friburgo no Estado do Rio de Janeiro e com 11 anos de idade ingressou no Colégio Anchieta dos jesuítas.'
+    ),
+    (
+        'Carmela Dutra',
+        'Carmela Leite Dutra, também conhecida como Santinha, foi a primeira-dama do país de 31 de janeiro de 1946 até a sua morte, tendo sido a esposa de Eurico Gaspar Dutra, 16.º presidente do Brasil.'
+    )
---
+++ routes/routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/DevCarlosOli/api-go-rest/controllers"
+	"github.com/gorilla/mux"
+)
+
+func HandleRequest() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasAsPersonalidades).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
