a61dc790dc2f044229336c693322861494c88975
Carlos Oliveira <c.oliveiradev@gmail.com>
2025-11-02 23:10:40 -0300

    feat: Incluindo middleware

--- routes/routes.go
+++ routes/routes.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 
 	"github.com/DevCarlosOli/api-go-rest/controllers"
+	"github.com/DevCarlosOli/api-go-rest/middleware"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasAsPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
